f2f3e69b0b93bd8fb486e95bb773679592cead2a
Chi Ng <benng10019@gmail.com>
2013-06-10 09:11:10 -0400

    added new test cases

--- knockout_eval.js
+++ knockout_eval.js
@@ -1,10 +1,17 @@
 $(function() {
     var viewModel = {
         name: "bob",
+        // nameVisible: ko.observable(true),
         changeName: function() {
             this.name = "ben";
         }
     };
     
+    // viewModel.displayName = ko.dependentObservable(function(){
+    //     return this.name() + " is" + (!this.nameVisible() ? "not" : "") + ' visible';
+    // }, viewModel);
+    
     ko.applyBindings(viewModel);//Important!
+    
+    console.log("TEST");
 });
